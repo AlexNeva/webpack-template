c215e301af69ccb28143d920f4909a17bd75004e
AlexNeva <sashnevski@yandex.ru>
2023-04-26 11:35:48 +0300

    refactoring

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.html";
-import "./style.css";
-
-const sum = (a, b) => a + b;
-
-console.log(sum(1, 3));
-console.log(sum(1, 5));
-console.log(sum(1, 2));
-console.log(sum(1, 7));
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,82 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const path = require('path');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+const getConfig = (mode, target, devtool) => {
+    const configs = [];
+
+    glob.sync(`./src/*/*.html`).forEach((file) => {
+        const folderPath = path.dirname(file);
+        const folderName = folderPath.replace('src\\', '');
+
+        // entries[folderName] = `./${folderPath}\\script.js`;
+
+        const config = {
+            mode,
+            target,
+            devtool,
+            entry: {
+                [folderName]: path.resolve(__dirname, 'src', folderName, 'script.js'),
+            },
+            output: {
+                path: path.resolve(__dirname, 'dist'),
+                clean: true,
+                filename: '[name].js',
+                publicPath: '/',
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: file,
+                    filename: '[name].html',
+                    inject: 'body',
+                    inlineSource: '.(js)$',
+                    cache: false,
+                }),
+                new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
+            ],
+            module: {
+                rules: [
+                    {
+                        test: /\.html$/i,
+                        loader: 'html-loader',
+                    },
+                    {
+                        test: /\.css$/i,
+                        use: [
+                            'style-loader',
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: { postcssOptions: { plugins: [require('postcss-preset-env')] } },
+                            },
+                        ],
+                    },
+                    {
+                        test: /\.(?:js|mjs|cjs)$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', { targets: 'defaults' }]],
+                            },
+                        },
+                    },
+                ],
+            },
+        };
+
+        configs.push(config);
+    });
+
+    return configs;
+};
 
 module.exports = (env) => {
-  const mode = env.production ? "production" : "development";
-  const devMode = mode === "development";
-  const target = devMode ? "web" : "browserslist";
-  const devtool = devMode && "source-map";
+    const mode = env.production ? 'production' : 'development';
+    const devMode = mode === 'development';
+    const target = devMode ? 'web' : 'browserslist';
+    const devtool = devMode && 'source-map';
 
-  return {
-    mode,
-    target,
-    devtool,
-    entry: path.resolve(__dirname, "src", "index.js"),
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-      filename: "index.[contenthash].js",
-      publicPath: "/",
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src", "index.html"),
-        inject: "body",
-        inlineSource: ".(js|css)$",
-      }),
-      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.html$/i,
-          loader: "html-loader",
-        },
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-  };
+    return getConfig(mode, target, devtool);
 };
